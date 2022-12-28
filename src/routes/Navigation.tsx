0e22f98d9ebce1d6780a761f28f7f24d0811f32b
jrp60 <javirodenas.javirodenas@gmail.com>
2022-12-28 01:10:16 +0100

    icons fixed in develop, camera

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -6,7 +6,6 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import DisplayScreen from '../components/screens/DisplayScreen';
 import CameraScreen from '../components/screens/CameraScreen';
-import Icon from 'react-native-ionicons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Stack = createNativeStackNavigator();
@@ -29,12 +28,14 @@ function MyTabs() {
         tabBarStyle: {backgroundColor: MyTheme.colors.background},
         tabBarActiveTintColor: MyTheme.colors.primary,
         tabBarInactiveTintColor: MyTheme.colors.inactiveTintColor,
+        headerStyle: {backgroundColor: MyTheme.colors.background},
       }}>
       <Tab.Screen
         name="HelloWorldScreen"
         component={HelloWorldScreen}
         options={{
           tabBarLabel: 'Home',
+
           tabBarIcon: ({color}) => (
             <Ionicons name="shirt" size={28} color={color} />
           ),
@@ -42,15 +43,16 @@ function MyTabs() {
       />
       <Tab.Screen
         name="Home"
-        component={HelloWorldScreen}
+        component={CameraScreen}
         options={{
-          tabBarLabel: 'Home',
+          tabBarLabel: 'Camera',
+          headerShown: false,
           tabBarIcon: ({color, size}) => (
-            <Ionicons name="shirt" size={28} color={color} />
+            <Ionicons name="camera" size={28} color={color} />
           ),
         }}
       />
-      <Tab.Screen name="Settings" component={CameraScreen} />
+      <Tab.Screen name="Settings" component={DisplayScreen} />
     </Tab.Navigator>
   );
 }
@@ -60,10 +62,11 @@ function Navigation() {
     <NavigationContainer theme={MyTheme}>
       {/* <SafeAreaView> */}
       <Stack.Navigator
-        initialRouteName="HelloWorldScreen"
+        initialRouteName="MyTabs"
         screenOptions={{
           headerShown: false,
         }}>
+        <Stack.Screen name="MyTabs" component={MyTabs} />
         <Stack.Screen name="HelloWorldScreen" component={HelloWorldScreen} />
         <Stack.Screen name="DisplayScreen" component={DisplayScreen} />
         <Stack.Screen name="CameraScreen" component={CameraScreen} />
